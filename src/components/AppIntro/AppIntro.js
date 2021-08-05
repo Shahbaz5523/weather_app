63b732a635a67aacaa4d034ea84036e814338b08
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2021-08-05 12:04:59 +0500

    stylling issue

---
+++ src/components/AppIntro/AppIntro.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+export default function AppIntro() {
+    return (
+        <div classsName="weathercontainer">
+            <h1>Weather App</h1>
+            <img src="img/men.svg" alt="illustration missing" />
+            <button>Check Todays Weather</button>
+        </div>
+    )
+}
