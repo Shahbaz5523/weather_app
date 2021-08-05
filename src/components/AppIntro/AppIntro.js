e9f1e2a7cfe2bea3971c77f661a34f798a03bcd9
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2021-08-05 14:43:19 +0500

    fix error and main page completed

--- src/components/AppIntro/AppIntro.js
+++ src/components/AppIntro/AppIntro.js
@@ -2,10 +2,19 @@ import './style.css';
 
 export default function AppIntro() {
     return (
-        <div classsName="weathercontainer">
+        <div>
+        <div className="weathercontainer">
+            <div className="header">
+                <span>Daily</span>
+                <span>Weather</span>
+            </div>
             <h1>Weather App</h1>
             <img src="img/men.svg" alt="illustration missing" />
-            <button>Check Todays Weather</button>
+        </div>
+            <div className="footer">
+                <button>Check Todays Weather</button>
+                <p>Copyright &copy; 2021 All Right Reservied.</p>
+            </div>
         </div>
     )
 }
